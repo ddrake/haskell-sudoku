1dafe624bf6bd71779a43f560147efd1ea01e1ea
dow <dowdrake@msn.com>
2014-07-10 22:07:13 -0700

    cleanup

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,10 +1,5 @@
 module Sudoku
-( initGame,
-  initg,
-  cellsForRow,
-  cellsForCol,
-  cellsForBox,
-  kv,
+( initg,
   gct,
   refine,
   cycl,
@@ -20,9 +15,40 @@ data Cell = Cell { row :: Int, col :: Int, possibles :: [Int] } deriving (Show, 
 instance Eq Cell where x == y = row x == row y && col x == col y
 type Game = [Cell]
 
-rows :: Game -> [[Cell]]
-rows = groupsOf 9
+-------------------------------------
+-- Primary Tools for Solving a Puzzle
+-------------------------------------
+cycl :: Game -> Game
+cycl game =
+    let ct = gct game
+        game' = refine . step2 . step1 $ game
+    in if gct game' > ct then cycl game' else game'
 
+guess :: Int -> Int -> Int -> Game -> Game
+guess r c v = setCellValue (r-1) (c-1) v
+
+-----------------
+-- Initialization
+-----------------
+initg :: String -> Game
+initg s = 
+    let inits = map (\x -> if x == '.' then 0 :: Int else read [x] :: Int) s
+    in initGame inits
+    
+-- Initialize a game from a list of Ints where the unknown values are represented by zeros
+initGame :: [Int] -> Game 
+initGame vals = 
+    let val row col = vals !! (gameIndex row col)
+        possibles row col = if val row col == 0 then [1..9] else [val row col]
+    in [Cell row col $ possibles row col | row <- [0..8], col <- [0..8] ]
+
+-- The index of a cell in the context of the overall 81 cell game
+gameIndex :: Int -> Int -> Int
+gameIndex row col = row  * 9 + col
+
+----------------
+-- Display Logic
+----------------
 sPoss :: [Int] -> String
 sPoss = centerTo 10 . map (\x -> head (show x)) 
 
@@ -38,39 +64,14 @@ centerTo n s =
         rest = n - length s - half
     in replicate half ' ' ++ s ++ replicate rest ' '
 
--- The index of a cell in the context of the overall 81 cell game
-gameIndex :: Int -> Int -> Int
-gameIndex row col = row  * 9 + col
+-----------------------
+-- 'No Duplicates' Rule
+-----------------------
+step1 :: Game -> Game
+step1 game = 
+    let possibles = map (newPossiblesForCell game) game
+    in zipWith (\c p -> Cell (row c) (col c) p) game possibles
 
--- Get the box for a cell
-box :: Cell -> Int
-box (Cell r c _) = 3 * (r `div` 3) + (c `div` 3)
-
--- Initialize a game from a list of Ints where the unknown values are represented by zeros
-initGame :: [Int] -> Game 
-initGame vals = 
-    let val row col = vals !! (gameIndex row col)
-        possibles row col = if val row col == 0 then [1..9] else [val row col]
-    in [Cell row col $ possibles row col | row <- [0..8], col <- [0..8] ]
-
-initg :: String -> Game
-initg s = 
-    let inits = map (\x -> if x == '.' then 0 :: Int else read [x] :: Int) s
-    in initGame inits
-    
-cellsForRow :: Int -> Game -> [Cell]
-cellsForRow r = filter (\cell -> row cell == r)
-
-cellsForCol :: Int -> Game -> [Cell]
-cellsForCol c = filter (\cell -> col cell == c)
-
-cellsForBox :: Int -> Game -> [Cell]
-cellsForBox b = filter (\cell -> box cell == b)
-
-kv :: [Cell] -> [Int]
-kv = concat . filter (\p -> length p == 1) . map possibles
-
--- Re-compute the possible values for a cell by applying the "no duplicates" rule
 newPossiblesForCell :: Game -> Cell -> [Int]
 newPossiblesForCell game cell@(Cell r c p)
     | length p == 1 = p
@@ -80,20 +81,32 @@ newPossiblesForCell game cell@(Cell r c p)
             boxValues = kv . cellsForBox (box cell) $ game 
         in (\\) (possibles cell) . union rowValues . union colValues $ boxValues
 
-step1 :: Game -> Game
-step1 game = 
-    let possibles = map (newPossiblesForCell game) game
-    in zipWith (\c p -> Cell (row c) (col c) p) game possibles
+-----------------------------------------
+-- 'Every Value (1-9) Must Be There' Rule
+--   If a cell has a possible value that is 
+--   not possible for any other cell in its row, 
+--   that value must be the value for the cell.
+-----------------------------------------
+step2 :: Game -> Game
+step2 game = 
+    let newCell c p = if length p == 1 then Cell (row c) (col c) p else c
+        uniques = uniquesForAllCells game
+    in zipWith (\c p -> newCell c p) game uniques
 
--- If a cell has a possible value that is not possible for any other cell in its row, 
--- that value must be the value for the cell.
 unionPossiblesForCells :: [Cell] -> [Int]
 unionPossiblesForCells cells = 
     let allPossible = map possibles cells
     in foldr (\x a -> union a x) [] allPossible
 
-uniquesForDisjointBlockAndCell :: [Cell] -> Cell -> [Int]
-uniquesForDisjointBlockAndCell block cell = (possibles cell) \\ (unionPossiblesForCells block)
+uniquesForAllCells :: Game -> [[Int]]
+uniquesForAllCells game = map (uniquesForCell game) game
+
+uniquesForCell :: Game -> Cell -> [Int]
+uniquesForCell game cell = 
+    let rowUniques = uniquesForRow game cell
+        colUniques = uniquesForCol game cell
+        boxUniques = uniquesForBox game cell
+    in union rowUniques . union colUniques $ boxUniques
 
 uniquesForRow :: Game -> Cell -> [Int]
 uniquesForRow game cell = 
@@ -110,58 +123,19 @@ uniquesForBox game cell =
     let block = delete cell . cellsForBox (box cell) $ game
     in uniquesForDisjointBlockAndCell block cell
 
-uniquesForCell :: Game -> Cell -> [Int]
-uniquesForCell game cell = 
-    let rowUniques = uniquesForRow game cell
-        colUniques = uniquesForCol game cell
-        boxUniques = uniquesForBox game cell
-    in union rowUniques . union colUniques $ boxUniques
+uniquesForDisjointBlockAndCell :: [Cell] -> Cell -> [Int]
+uniquesForDisjointBlockAndCell block cell = (possibles cell) \\ (unionPossiblesForCells block)
 
-uniquesForAllCells :: Game -> [[Int]]
-uniquesForAllCells game = map (uniquesForCell game) game
-
--- For any cells whose possibles can be replaced by a singleton 
--- based on the "all nine numbers must be present" rule, perform the replacemnt. 
-step2 :: Game -> Game
-step2 game = 
-    let newCell c p = if length p == 1 then Cell (row c) (col c) p else c
-        uniques = uniquesForAllCells game
-    in zipWith (\c p -> newCell c p) game uniques
-
+--------------------------
+-- Double-Pairs Refinement
+--------------------------
 -- Check a row (col, box) for double pairs.  There may be multiple double pairs.  
--- If so, we'll need to handle them separately
-dpsForBlock ::[Cell] -> [Cell]
-dpsForBlock = filter (\c -> length (possibles c) == 2)
-
--- Update a cell from a list of values
-removePossibles :: [Int] -> Cell -> Cell
-removePossibles vals cell = Cell (row cell) (col cell) (possibles cell \\ vals)
-
--- General Purpose Helper: Break a list into n-sized chunks
-groupsOf :: Int -> [a] -> [[a]]
-groupsOf _ [] = []
-groupsOf 0 list = [list]
-groupsOf n list = take n list : groupsOf n (drop n list)
--- General Purpose Helper: Given a value and a list, return the corresponding value from the list
--- if it's there, otherwise the original value
-listItemOrOriginal :: (Eq a) => [a] -> a -> a
-listItemOrOriginal list item =  
-    case find (item ==) list of
-        Nothing -> item
-        Just listItem -> listItem
-
-gct :: Game -> Int
-gct = length . kv
-
-partitionDps :: [Cell] -> [[Cell]]
-partitionDps = filter (\x -> length x == 2) . groupBy ((==) `on` possibles) . sortBy (compare `on` possibles) . dpsForBlock
-
-refineBlock :: [Cell] -> [Cell] -> Game -> Game
-refineBlock cells block=
-    let vals = possibles (head cells)
-        restOfBlock = foldr (\x a -> delete x a) block cells
-        updatedRest = map (\c -> removePossibles vals c) restOfBlock
-    in map (\c -> listItemOrOriginal updatedRest c)
+-- If so, handle them separately
+refine :: Game -> Game
+refine game = 
+    let gameR = foldr (\x a -> refineRow x a) game [0..8]
+        gameC = foldr (\x a -> refineCol x a) gameR [0..8]
+    in foldr (\x a -> refineBox x a) gameC [0..8]
 
 refineRow :: Int -> Game -> Game
 refineRow row game = 
@@ -181,31 +155,82 @@ refineBox box game =
         partitioned = partitionDps boxCells
     in foldr (\x a -> refineBlock x boxCells a) game partitioned
 
-cycl :: Game -> Game
-cycl game =
-    let ct = gct game
-        game' = refine . step2 . step1 $ game
-    in if gct game' > ct then cycl game' else game'
+refineBlock :: [Cell] -> [Cell] -> Game -> Game
+refineBlock cells block=
+    let vals = possibles (head cells)
+        restOfBlock = foldr (\x a -> delete x a) block cells
+        updatedRest = map (\c -> removePossibles vals c) restOfBlock
+    in map (\c -> listItemOrOriginal updatedRest c)
 
-refine :: Game -> Game
-refine game = 
-    let gameR = foldr (\x a -> refineRow x a) game [0..8]
-        gameC = foldr (\x a -> refineCol x a) gameR [0..8]
-    in foldr (\x a -> refineBox x a) gameC [0..8]
+partitionDps :: [Cell] -> [[Cell]]
+partitionDps = filter (\x -> length x == 2) . groupBy ((==) `on` possibles) . sortBy (compare `on` possibles) . dpsForBlock
+
+dpsForBlock ::[Cell] -> [Cell]
+dpsForBlock = filter (\c -> length (possibles c) == 2)
+
+-- Remove specified values from the possibles for a cell
+removePossibles :: [Int] -> Cell -> Cell
+removePossibles vals cell = Cell (row cell) (col cell) (possibles cell \\ vals)
+
+-----------------
+-- Sudoku Helpers
+-----------------
+
+-- Get the box for a cell
+box :: Cell -> Int
+box (Cell r c _) = 3 * (r `div` 3) + (c `div` 3)
+
+cellsForRow :: Int -> Game -> [Cell]
+cellsForRow r = filter (\cell -> row cell == r)
+
+cellsForCol :: Int -> Game -> [Cell]
+cellsForCol c = filter (\cell -> col cell == c)
+
+cellsForBox :: Int -> Game -> [Cell]
+cellsForBox b = filter (\cell -> box cell == b)
 
 setCellValue :: Int -> Int -> Int -> Game -> Game
 setCellValue r c v = map (\cell -> if Cell r c [v] == cell then Cell (row cell) (col cell) [v] else cell)
 
-guess :: Int -> Int -> Int -> Game -> Game
-guess r c v = setCellValue (r-1) (c-1) v
+-- Known values for the game
+kv :: [Cell] -> [Int]
+kv = concat . filter (\p -> length p == 1) . map possibles
+
+-- Count of known values for the game
+gct :: Game -> Int
+gct = length . kv
+
+-- Useful for display
+rows :: Game -> [[Cell]]
+rows = groupsOf 9
+
+--------------------------
+-- General Purpose Helpers
+--------------------------
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf _ [] = []
+groupsOf 0 list = [list]
+groupsOf n list = take n list : groupsOf n (drop n list)
+
+-- Given a value and a list, return the corresponding value from the list
+listItemOrOriginal :: (Eq a) => [a] -> a -> a
+listItemOrOriginal list item =  
+    case find (item ==) list of
+        Nothing -> item
+        Just listItem -> listItem
 
 -- Sample usage
 --let g = initg ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
 --let g' = cycl g
---let g = refine g'
---gct g' ---> 40 (not there yet)
---let g = refine g'
---gct g  ---> 41 (found one using double-pairs)
---let g' = cycl g
---gct g ---> 81  :-)
---kv ---> 'known values' == the solution
+--gct g' ---> 56 (less than 81, so we're not there yet)
+--pretty g' -- stuck! better take a look at the game...
+
+--let g1 = guess 2 4 4 g'  -- naughty! - shouldn't be guessing!  (better to use logic)
+--let g1' = refine g1
+--gct g1' ---> 80 huh???
+--pretty g1' -- oh, I see, an empty set of possibles indicates that the guess was wrong!
+
+--let g1 = guess 2 4 9 g' -- maybe we'll get it this time, eh?
+--let g1' = refine g1'
+--gct g1'  ---> 81 -- yay - guessed right this time! -- genius!!
+--pretty g' -- ah, that's better
