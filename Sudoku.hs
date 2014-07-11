20404a05fa5acd4cbbf4543bcb298dcafbb50ae0
dow <dowdrake@msn.com>
2014-07-10 21:23:53 -0700

    added pretty printing of the game and guessing feature

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,12 +1,14 @@
 module Sudoku
 ( initGame,
+  initg,
   cellsForRow,
   cellsForCol,
   cellsForBox,
-  knownValues,
+  kv,
   gct,
-  refineGame,
-  cycleWhileImproving,
+  refine,
+  cycl,
+  pretty,
   Cell(..),
   Game
 ) where
@@ -17,6 +19,24 @@ import Data.Function
 data Cell = Cell { row :: Int, col :: Int, possibles :: [Int] } deriving (Show, Read)
 instance Eq Cell where x == y = row x == row y && col x == col y
 type Game = [Cell]
+
+rows :: Game -> [[Cell]]
+rows = groupsOf 9
+
+sPoss :: [Int] -> String
+sPoss = centerTo 10 . map (\x -> head (show x)) 
+
+allPoss :: [Cell] -> String
+allPoss row = concat . map (\c -> sPoss (possibles c)) $ row
+
+pretty :: Game -> IO ()
+pretty game = putStr . unlines . map allPoss $ rows game
+
+centerTo :: Int -> String -> String
+centerTo n s = 
+    let half = (n - length s) `div` 2
+        rest = n - length s - half
+    in replicate half ' ' ++ s ++ replicate rest ' '
 
 -- The index of a cell in the context of the overall 81 cell game
 gameIndex :: Int -> Int -> Int
@@ -33,8 +53,8 @@ initGame vals =
         possibles row col = if val row col == 0 then [1..9] else [val row col]
     in [Cell row col $ possibles row col | row <- [0..8], col <- [0..8] ]
 
-initFromString :: String -> Game
-initFromString s = 
+initg :: String -> Game
+initg s = 
     let inits = map (\x -> if x == '.' then 0 :: Int else read [x] :: Int) s
     in initGame inits
     
@@ -47,17 +67,17 @@ cellsForCol c = filter (\cell -> col cell == c)
 cellsForBox :: Int -> Game -> [Cell]
 cellsForBox b = filter (\cell -> box cell == b)
 
-knownValues :: [Cell] -> [Int]
-knownValues = concat . filter (\p -> length p == 1) . map possibles
+kv :: [Cell] -> [Int]
+kv = concat . filter (\p -> length p == 1) . map possibles
 
 -- Re-compute the possible values for a cell by applying the "no duplicates" rule
 newPossiblesForCell :: Game -> Cell -> [Int]
 newPossiblesForCell game cell@(Cell r c p)
     | length p == 1 = p
     | otherwise =
-        let rowValues = knownValues . cellsForRow (row cell) $ game 
-            colValues = knownValues . cellsForCol (col cell) $ game 
-            boxValues = knownValues . cellsForBox (box cell) $ game 
+        let rowValues = kv . cellsForRow (row cell) $ game 
+            colValues = kv . cellsForCol (col cell) $ game 
+            boxValues = kv . cellsForBox (box cell) $ game 
         in (\\) (possibles cell) . union rowValues . union colValues $ boxValues
 
 step1 :: Game -> Game
@@ -117,6 +137,11 @@ dpsForBlock = filter (\c -> length (possibles c) == 2)
 removePossibles :: [Int] -> Cell -> Cell
 removePossibles vals cell = Cell (row cell) (col cell) (possibles cell \\ vals)
 
+-- General Purpose Helper: Break a list into n-sized chunks
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf _ [] = []
+groupsOf 0 list = [list]
+groupsOf n list = take n list : groupsOf n (drop n list)
 -- General Purpose Helper: Given a value and a list, return the corresponding value from the list
 -- if it's there, otherwise the original value
 listItemOrOriginal :: (Eq a) => [a] -> a -> a
@@ -126,7 +151,7 @@ listItemOrOriginal list item =
         Just listItem -> listItem
 
 gct :: Game -> Int
-gct = length . knownValues
+gct = length . kv
 
 partitionDps :: [Cell] -> [[Cell]]
 partitionDps = filter (\x -> length x == 2) . groupBy ((==) `on` possibles) . sortBy (compare `on` possibles) . dpsForBlock
@@ -156,25 +181,31 @@ refineBox box game =
         partitioned = partitionDps boxCells
     in foldr (\x a -> refineBlock x boxCells a) game partitioned
 
-cycleWhileImproving :: Game -> Game
-cycleWhileImproving game =
+cycl :: Game -> Game
+cycl game =
     let ct = gct game
-        game' = step2 . step1 $ game
-    in if gct game' > ct then cycleWhileImproving game' else game'
+        game' = refine . step2 . step1 $ game
+    in if gct game' > ct then cycl game' else game'
 
-refineGame :: Game -> Game
-refineGame game = 
+refine :: Game -> Game
+refine game = 
     let gameR = foldr (\x a -> refineRow x a) game [0..8]
         gameC = foldr (\x a -> refineCol x a) gameR [0..8]
     in foldr (\x a -> refineBox x a) gameC [0..8]
 
+setCellValue :: Int -> Int -> Int -> Game -> Game
+setCellValue r c v = map (\cell -> if Cell r c [v] == cell then Cell (row cell) (col cell) [v] else cell)
+
+guess :: Int -> Int -> Int -> Game -> Game
+guess r c v = setCellValue (r-1) (c-1) v
+
 -- Sample usage
---let inits = [5,0,0, 0,0,6, 0,7,0,  0,4,0, 8,0,0, 0,0,0,  0,8,1, 4,0,5, 0,0,2,  0,0,0, 0,0,0, 7,0,0,  7,0,2, 0,0,0, 4,0,9,  0,0,4, 0,0,0, 0,0,0,  4,0,0, 6,0,1, 3,5,0,   0,0,0, 0,0,4, 0,6,0,  0,3,0, 5,0,0, 0,0,8] :: [Int]
---let g = initGame inits
---let g' = cycleWhileImproving g
+--let g = initg ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
+--let g' = cycl g
+--let g = refine g'
 --gct g' ---> 40 (not there yet)
---let g = refineGame g'
+--let g = refine g'
 --gct g  ---> 41 (found one using double-pairs)
---let g' = cycleWhileImproving g
+--let g' = cycl g
 --gct g ---> 81  :-)
---knownValues ---> the solution
+--kv ---> 'known values' == the solution
---
+++ readme.md
@@ -0,0 +1,61 @@
+Haskell Sudoku
+==============
+
+A Sudoku solver in Haskell
+
+Sample Session
+--------------
+
+~~~
+dow@dow-laptop ~/haskell/sudoku $ ghci
+GHCi, version 7.4.1: http://www.haskell.org/ghc/  :? for help
+Loading package ghc-prim ... linking ... done.
+Loading package integer-gmp ... linking ... done.
+Loading package base ... linking ... done.
+Prelude> :l Sudoku
+[1 of 1] Compiling Sudoku           ( Sudoku.hs, interpreted )
+Ok, modules loaded: Sudoku.
+*Sudoku> let g = initg ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
+*Sudoku> let g' = cycl g
+*Sudoku> gct g'
+56
+*Sudoku> pretty g'
+    7         9         4         5         8         2         1         3         6     
+    2         6         58        49        3         1         7         48        59    
+   1358       15       1358       49        7         6         59        48        2     
+    6         8         59        7         1         59        3         2         4     
+    4         3         2         8         6         59        59        7         1     
+   159        15        7         2         4         3         8         6         59    
+   189        2        189        6         5         7         4         19        3     
+   359        4        359        1         2         8         6         59        7     
+    15        7         6         3         9         4         2         15        8     
+*Sudoku> let g1 = guess 2 4 4 g'
+*Sudoku> let g1' = cycl g1
+*Sudoku> gct g1'
+80
+*Sudoku> pretty g1'
+    7         9         4         5         8         2         1         3         6     
+    2         6         5         4         3         1         7         8         9     
+    3         1         8         9         7         6         5         4         2     
+    6         8                   7         1         9         3         2         4     
+    4         3         2         8         6         5         9         7         1     
+    9         1         7         2         4         3         8         6         5     
+    8         2         1         6         5         7         4         9         3     
+    3         4         9         1         2         8         6         5         7     
+    5         7         6         3         9         4         2         1         8     
+*Sudoku> let g1 = guess 2 4 9 g'
+*Sudoku> let g1' = cycl g1
+*Sudoku> gct g1'
+81
+*Sudoku> pretty g1'
+    7         9         4         5         8         2         1         3         6     
+    2         6         8         9         3         1         7         4         5     
+    3         1         5         4         7         6         9         8         2     
+    6         8         9         7         1         5         3         2         4     
+    4         3         2         8         6         9         5         7         1     
+    1         5         7         2         4         3         8         6         9     
+    8         2         1         6         5         7         4         9         3     
+    9         4         3         1         2         8         6         5         7     
+    5         7         6         3         9         4         2         1         8     
+*Sudoku> 
+~~~
